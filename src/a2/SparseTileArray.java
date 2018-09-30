59936d2c813e83df21f643f6f2c8608dc65f94c0
Fan <VIP.Fan.Deng@gmail.com>
2018-09-30 11:44:33 +0800

    SparseTileArray

--- src/a2/SparseTileArray.java
+++ src/a2/SparseTileArray.java
@@ -2,23 +2,28 @@ package a2;
 
 import csse2002.block.world.Tile;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Administrator on 2018/9/28.
  */
 public class SparseTileArray {
+    private Map<Position, Tile> map;
+    private List<Tile> tiles;
+
     public SparseTileArray() {
+        map = new TreeMap<>();
+        tiles = new LinkedList<>();
     }
 
     public void addLinkedTiles(Tile startingTile, int startingX, int startingY) throws WorldMapInconsistentException {
     }
 
     public Tile getTile(Position position) {
-        return null;
+        return map.get(position);
     }
 
     public List<Tile> getTiles() {
-        return null;
+        return Collections.unmodifiableList(tiles);
     }
 }
