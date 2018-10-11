c7829b001eae5214d649eee3d1e2f2069ffa4102
VIP.Fan.Deng@gmail.com <Vip961028>
2018-10-11 23:48:29 +0800

    右边布局

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -2,15 +2,14 @@ package gui;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -19,7 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 300, 250, Color.WHITE);
+        Scene scene = new Scene(root, 600, 450, Color.WHITE);
 
         MenuBar menuBar = new MenuBar();
         menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
@@ -65,6 +64,74 @@ public class Main extends Application {
         sqlMenu.getItems().add(tutorialManeu);
 
         menuBar.getMenus().addAll(fileMenu, webMenu, sqlMenu);
+
+        //direction area
+        BorderPane dirPane = new BorderPane();
+        dirPane.setPadding(new Insets(65,60,110,0));
+        root.setRight(dirPane);
+
+        //north button
+        Button northButton = new Button("north");
+        northButton.setPrefSize(70,40);
+        northButton.setStyle("-fx-background-color: #336699");
+        //east button
+        Button eastButton = new Button("east");
+        eastButton.setPrefSize(70,40);
+        eastButton.setStyle("-fx-background-color: #336699");
+        //south button
+        Button southButton = new Button("south");
+        southButton.setPrefSize(70,40);
+        southButton.setStyle("-fx-background-color: #336699");
+        //west button
+        Button westButton = new Button("west");
+        westButton.setPrefSize(70,40);
+        westButton.setStyle("-fx-background-color: #336699");
+        //dig button
+        Button digButton = new Button("Dig");
+        digButton.setStyle("-fx-background-color: #336699");
+        //drop button
+        Button dropButton = new Button("Drop");
+        dropButton.setStyle("-fx-background-color: #336699");
+        //move choice
+        ObservableList cursors = FXCollections.observableArrayList("Move Builder","Move Block");
+        ChoiceBox choiceBox = new ChoiceBox();
+        choiceBox.setItems(cursors);
+        choiceBox.getSelectionModel().select(0);
+        choiceBox.setPrefWidth(110);
+        //drop Index textField
+        TextField dropIndexField = new TextField();
+        dropIndexField.setPrefWidth(140);
+        dropIndexField.setPromptText("Drop Index");
+
+        //north and south
+        VBox cenBox = new VBox();
+        cenBox.setSpacing(40);
+        cenBox.getChildren().addAll(northButton,southButton);
+        dirPane.setCenter(cenBox);
+        //west
+        VBox leftBox = new VBox();
+        leftBox.setPadding(new Insets(40,0,0,0));
+        leftBox.getChildren().add(westButton);
+        dirPane.setLeft(leftBox);
+        //east
+        VBox rightBox = new VBox();
+        rightBox.getChildren().add(eastButton);
+        rightBox.setPadding(new Insets(40,0,0,0));
+        dirPane.setRight(rightBox);
+
+        //choiceBox,digButton,dropButton and dropIndexField
+        VBox otherBox = new VBox();
+        otherBox.setSpacing(20);
+        HBox choiceHBox = new HBox();
+        choiceHBox.setPadding(new Insets(0,0,0,50));
+        choiceHBox.getChildren().add(choiceBox);
+        HBox digHBox = new HBox();
+        digHBox.getChildren().add(digButton);
+        HBox dropHBox = new HBox();
+        dropHBox.setSpacing(5);
+        dropHBox.getChildren().addAll(dropButton,dropIndexField);
+        otherBox.getChildren().addAll(choiceHBox,digHBox,dropHBox);
+        dirPane.setBottom(otherBox);
 
         primaryStage.setScene(scene);
         primaryStage.show();
